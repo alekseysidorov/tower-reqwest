04d63d575667a455e4a0d275d430cd4cc9c9c41e
Aleksey Sidorov <sauron1987@gmail.com>
2024-04-21 16:48:06 +0300

    Try to fix symlinks

--- tower-http-client/src/lib.rs
+++ tower-http-client/src/lib.rs
@@ -1,6 +1,8 @@
 #![cfg_attr(docsrs, feature(doc_auto_cfg, doc_cfg))]
 
 //! # Overview
+//!
+#![doc = include_utils::include_md!("README.md:description")]
 
 #[cfg(feature = "util")]
 #[cfg_attr(docsrs, doc(cfg(feature = "util")))]
