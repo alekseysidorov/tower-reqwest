a0eeb942cd6ed31d76ffed61e0d59f9db74fd838
Aleksey Sidorov <sauron1987@gmail.com>
2024-07-16 19:02:07 +0300

    Add retry example

--- tower-http-client/src/adapters.rs
+++ tower-http-client/src/adapters.rs
@@ -5,5 +5,5 @@
 /// [`reqwest`]: https://crates.io/crates/reqwest
 #[cfg(feature = "reqwest")]
 pub mod reqwest {
-    pub use tower_reqwest::{error, HttpClientLayer, HttpClientService};
+    pub use tower_reqwest::{error, into_reqwest_body, HttpClientLayer, HttpClientService};
 }
