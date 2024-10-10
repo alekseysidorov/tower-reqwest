b0281b0f16f275d973b4dd9d6b0652d0cde9d520
Aleksey Sidorov <sauron1987@gmail.com>
2024-10-10 18:20:18 +0300

    Add deprecation warning

--- tower-http-client/src/adapters.rs
+++
@@ -1,9 +0,0 @@
-//! Adapters for various clients.
-
-/// Adapter for [`reqwest`] client.
-///
-/// [`reqwest`]: https://crates.io/crates/reqwest
-#[cfg(feature = "reqwest")]
-pub mod reqwest {
-    pub use tower_reqwest::{error, into_reqwest_body, HttpClientLayer, HttpClientService};
-}
